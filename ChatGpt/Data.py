ed616583bb81e7a2a2cf477d1ed009ed97f3e2f4
Erwin Jacimino <erwinjacimino2003@gmail.com>
2023-07-22 10:59:54 -0400

    HUBO CAMBIOS EN LA INTERFAZ DE OPENAI POR LO CUAL SE NECESITO UNA UPDATE

--- ChatGpt/Data.py
+++ ChatGpt/Data.py
@@ -2,12 +2,12 @@ PRIMERBOTON = '//*[@id="radix-:re:"]/div[2]/div[1]/div[2]/button'
 SEGUNDOBOTON = '//*[@id="radix-:re:"]/div[2]/div[1]/div[2]/button[2]'
 INPUTTEXT = '//*[@id="prompt-textarea"]'
 
-GPTREQUEST = '//*[@id="__next"]/div[1]/div[2]/div/main/div[2]/div/div/div/div[position() mod 2 = 0]'
+GPTREQUEST = '//*[@id="__next"]/div[1]/div[2]/div/main/div[1]/div/div/div/div[position() mod 2 = 0]'
 CONTAINERTEXT = '/div/div[2]/div[1]/div/div'
 
 
 
-CHECKFULL = '//*[@id="__next"]/div[1]/div[2]/div/main/div[3]/form/div/div[1]/div/button'
+CHECKFULL = '//*[@id="__next"]/div[1]/div[2]/div/main/div[2]/form/div/div[1]/div/button'
 
 stealth_js = '''() => {
         Object.defineProperty(navigator, 'webdriver', {
